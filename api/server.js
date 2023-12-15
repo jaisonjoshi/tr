f4f51cbba3deadfea087c760873373d22a2ac542
jaisjo <jaisonjoshi2001@gmail.com>
2023-12-15 18:30:31 +0530

    initial commit

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://localhost:3000",
+      "https://trouvailler.com", "https://admin.trouvailler.com"
     ],
     credentials: true,
   })
